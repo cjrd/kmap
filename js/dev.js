3b5aafe025ab59ffc5da3bb822bd8effb592f6ae
Colorado Reed <colorado.j.reed@gmail.com>
2014-01-03 19:17:29 -0800

    added meta demo data and workable dev.js

--- js/dev.js
+++ js/dev.js
@@ -7,28 +7,28 @@ require(["main"], function(KMap){
       graphView = new KMap.GraphView({model: graphModel, includeShortestDep: true}),
       graphListView = new KMap.GraphListView({model: graphModel});
 
-  // fetch some graph data
-  $.getJSON("/data/demo_kmap.json",
-    function (data) {
+  var handleDataFun = function (data) {
 
-      // add the data to the graph model
-      graphModel.addJsonNodesToGraph(data);
+    // add the data to the graph model
+    graphModel.addJsonNodesToGraph(data);
 
-      // set the graph placement (don't use if "x" and "y" are specified in the data)
-      graphView.optimizeGraphPlacement(false, false);
+    // set the graph placement (don't use if "x" and "y" are specified in the data)
+    graphView.optimizeGraphPlacement(false, false);
 
-      // render the views
-      graphView.render();
-      graphView.centerForNode(graphModel.getNode("F-62"));
-      graphListView.render();
+    // render the views
+    graphView.render();
+    graphView.centerForNode(graphModel.getNode(graphModel.getTopoSort().shift()));
+    graphListView.render();
 
-      // insert them into the html
-      $("body").prepend(graphListView.$el);
-      $("#graph-view-wrapper").append(graphView.$el);
+    // insert them into the html
+    $("body").prepend(graphListView.$el);
+    $("#graph-view-wrapper").append(graphView.$el);
+  };
+
+  // fetch some graph data (multiple fetches since demo_kmap is not stored in repo yet
+  $.getJSON("/data/demo_kmap.json", handleDataFun)
+    .fail(function () {
+      $.getJSON("/data/metacademy_demo.json", handleDataFun);
     });
-
-  // add the data to the graph model
-
-  // render the views and insert them in a desired location
 
 });
